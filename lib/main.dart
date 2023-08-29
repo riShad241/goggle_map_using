5faf370c39bb6a1b874b43d00c0538e0fd53d8b5
riShad241 <Kamorgaming@gmail.com>
2023-08-29 09:20:56 +0600

    Real Time Location Tracker

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:location/location.dart';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -22,15 +26,125 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
+  LocationData? myCurrentLocation;
   late final GoogleMapController _googleMapController;
+  StreamSubscription<LocationData>? _locationSubscription;
+  final Location _location = Location();
+  List<LatLng> _polylineCoordinates = []; // List to store coordinates
+  Polyline _polyline = Polyline(polylineId: PolylineId('user_route'));
+  Marker? _userMarker;
+
+  void getMyLocation() async{
+    await Location.instance.requestPermission().then((requestpermission){
+      print(requestpermission);
+    });
+    await Location.instance.hasPermission().then((permissionStatus){
+      print(permissionStatus);
+    });
+    myCurrentLocation = await Location.instance.getLocation();
+    print(myCurrentLocation);
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Future<void> _initalizeMap()async{
+    final location = Location();
+    /// Create and request location permission
+    if(await location.hasPermission() == PermissionStatus.denied){
+      await location.requestPermission();
+    }
+
+    ///get Current location
+     final currentLocation = await location.getLocation();
+    /// Animated the map to the current location
+    _googleMapController?.animateCamera(CameraUpdate.newLatLngZoom(
+        LatLng(currentLocation.latitude!, currentLocation.longitude!), 15));
+
+    setState(() {
+      myCurrentLocation = currentLocation;
+    });
+  }
+  void _startLocationUpdates() {
+    _locationSubscription = _location.onLocationChanged.listen((LocationData locationData) {
+      _updateMarkerPosition(locationData);
+      _updatePolyline(locationData);
+      _googleMapController?.animateCamera(CameraUpdate.newLatLng(
+        LatLng(locationData.latitude!, locationData.longitude!),
+      ));
+    });
+  }
+
+  void _updateMarkerPosition(LocationData locationData) {
+    setState(() {
+      myCurrentLocation = locationData;
+
+      if (_userMarker == null) {
+        _userMarker = Marker(
+          markerId: MarkerId('current_location'),
+          position: LatLng(locationData.latitude!, locationData.longitude!),
+          onTap: () {
+          },
+        );
+      } else {
+        _userMarker = _userMarker!.copyWith(
+          positionParam: LatLng(locationData.latitude!, locationData.longitude!),
+        );
+      }
+    });
+
+    // Request a re-render of the map to update marker position
+    _googleMapController?.moveCamera(CameraUpdate.newLatLng(
+      LatLng(locationData.latitude!, locationData.longitude!),
+    ));
+  }
+
+  void _showInfoWindow(LocationData locationData) {
+    final infoWindow = InfoWindow(
+      title: 'My Current Location',
+      snippet: 'Lat: ${locationData.latitude}, Lng: ${locationData.longitude}',
+    );
+    _googleMapController?.showMarkerInfoWindow(MarkerId('current_location'));
+
+  }
+
+  void _updatePolyline(LocationData locationData) {
+    setState(() {
+      _polylineCoordinates.add(LatLng(locationData.latitude!, locationData.longitude!));
+      _polyline = Polyline(
+        polylineId: PolylineId('user_route'),
+        color: Colors.blue,
+        points: _polylineCoordinates,
+      );
+      _googleMapController?.animateCamera(CameraUpdate.newLatLngBounds(
+        LatLngBounds(southwest: _polylineCoordinates.first, northeast: _polylineCoordinates.last),
+        100, // Padding value to ensure the polyline is visible
+      ));
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initalizeMap();
+    _startLocationUpdates();
+
+  }
+  @override
+  void dispose() {
+    _locationSubscription?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Goggle Map Screen'),
       ),
-      body: GoogleMap(
+      body: Stack(
+        children: [
+      GoogleMap(
         initialCameraPosition: const CameraPosition(
           zoom: 15,
           bearing: 30,
@@ -47,123 +161,45 @@ class _HomeScreenState extends State<HomeScreen> {
           _googleMapController = controller;
         },
         compassEnabled: true,
-        onTap: (LatLng l) {
-          print(l);
-        },
-        onLongPress: (LatLng l) {
-          print(l);
-        },
         mapType: MapType.normal,
-        markers: <Marker>{
-          Marker(
-              markerId: MarkerId('custom-marker'),
-              position: LatLng(24.10648361418759, 89.96545615056893),
-              icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-              infoWindow: InfoWindow(title: 'Bazar'),
-              draggable: false,
-              // draggable: true,
-              onDragStart: (LatLng latLng) {
-                print(latLng);
-              },
-              onDragEnd: (LatLng latLng) {
-                print(latLng);
-              }
-          ),
-          Marker(
-              markerId: MarkerId('custom-marker-1'),
-              position: LatLng(24.109068980277137, 89.97399630377993),
-              icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueYellow),
-              infoWindow: InfoWindow(title: 'Bazar'),
-              draggable: false,
-              // draggable: true,
-              onDragStart: (LatLng latLng) {
-                print(latLng);
-              },
-              onDragEnd: (LatLng latLng) {
-                print(latLng);
-              }
-          ),
-          Marker(
-              markerId: MarkerId('custom-marker-2'),
-              position: LatLng(24.10152818327614, 89.96747317167907),
-              icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange),
-              infoWindow: InfoWindow(title: 'Bazar'),
-              draggable: false,
-              // draggable: true,
-              onDragStart: (LatLng latLng) {
-                print(latLng);
-              },
-              onDragEnd: (LatLng latLng) {
-                print(latLng);
-              }
-          ),
-        },
-        polylines: <Polyline> {
-          Polyline(polylineId: PolylineId('polyline'),
-              color: Colors.pink,
-              width: 20,
-              jointType: JointType.round,
-              onTap: (){
-                print('Tapped on polyline');
-              },
-              points: [
-                LatLng(24.10648361418759, 89.96545615056893),
-                LatLng(24.109068980277137, 89.97399630377993),
-                LatLng(24.10152818327614, 89.96747317167907),
-              ]),
-        },
-        circles: <Circle> {
-          Circle(
-              circleId: CircleId('demo'),
-              center: LatLng(24.10648361418759, 89.96545615056893),
-              radius: 500,
-              strokeColor: Colors.purple,
-              strokeWidth: 4,
-              fillColor: Colors.purple.shade100,
-              onTap: () {
-                print('Tapped on circle');
-              }
-          ),
-          Circle(
-              circleId: CircleId('demo-1'),
-              center:  LatLng(24.109068980277137, 89.97399630377993),
-              radius: 500,
-              strokeColor: Colors.deepOrange,
-              strokeWidth: 4,
-              fillColor: Colors.deepOrange.shade100,
-              onTap: () {
-                print('Tapped on circle');
-              }
-          ),
-          Circle(
-              circleId: CircleId('demo-3'),
-              center: LatLng(24.10152818327614, 89.96747317167907),
-              radius: 500,
-              strokeColor: Colors.blue,
-              strokeWidth: 4,
-              fillColor: Colors.blue.shade100,
-              onTap: () {
-                print('Tapped on circle');
-              }
-          ),
-        },
-        polygons: <Polygon> {
-          Polygon(polygonId: PolygonId('poly-id'),
-              fillColor: Colors.purple.shade400,
-              strokeWidth: 0,
-              onTap: () {
-              },
-              points: [
-                LatLng(24.094457058310464, 89.97290196279874),
-                LatLng(24.088521713190666, 89.97790160028127),
-                LatLng(24.0858575745658, 89.98912396241529),
-                LatLng(24.0796279753388, 89.98309435685475),
-                LatLng(24.066861184161237, 89.9845824598033),
-                LatLng(24.064235780991748, 89.97503379603725),
-              ]
-          )
-        },
+        markers: _userMarker != null ? Set<Marker>.of([_userMarker!]) : {},
+        polylines: _polyline != null ? Set<Polyline>.of([_polyline]) : {},
       ),
+      myCurrentLocation!= null
+          ? Positioned(
+        top: 16,
+        left: 16,
+        child: Container(
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Latitude: ${myCurrentLocation!.latitude}',
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Longitude: ${myCurrentLocation!.longitude}',
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      )
+          : SizedBox.shrink(),
+      ],
+    ),
+
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: (){
+      //     getMyLocation();
+      //   },
+      //   child: Icon(Icons.my_location),
+      // ),
     );
   }
 }
